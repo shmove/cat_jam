3d545ca4050b29ed503c2d908eb2bbb29dee0795
shmove <leahlddl@gmail.com>
2023-06-17 12:24:48 +0100

    Add template for modded discs (v1.1.0)

--- fabric/src/main/java/com/shmove/cat_jam/fabric/cat_jam_fabric.java
+++ fabric/src/main/java/com/shmove/cat_jam/fabric/cat_jam_fabric.java
@@ -1,13 +1,32 @@
 package com.shmove.cat_jam.fabric;
 
 import com.shmove.cat_jam.cat_jam;
+import com.shmove.cat_jam.helpers.discs.Disc;
+import com.shmove.cat_jam.helpers.discs.DiscSegment;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.util.List;
+
+import static com.shmove.cat_jam.cat_jam.discManager;
 
 public class cat_jam_fabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
         cat_jam.init();
+        initialiseModdedDiscs();
+    }
+
+    private void initialiseModdedDiscs() {
+
+        // Mod List
+
+        /* final boolean example_mod = FabricLoader.getInstance().isModLoaded("example_mod"); */
+
+        // Modded Discs
+
+        /* if (example_mod) discManager.addDisc(new Disc("example_mod:example_disc", 0, 0)); */
     }
 
 }
--- forge/src/main/java/com/shmove/cat_jam/forge/cat_jam_forge.java
+++ forge/src/main/java/com/shmove/cat_jam/forge/cat_jam_forge.java
@@ -1,13 +1,33 @@
 package com.shmove.cat_jam.forge;
 
 import com.shmove.cat_jam.cat_jam;
+import com.shmove.cat_jam.helpers.discs.Disc;
+import com.shmove.cat_jam.helpers.discs.DiscSegment;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+import static com.shmove.cat_jam.cat_jam.discManager;
 
 @Mod(cat_jam.MOD_ID)
 public class cat_jam_forge {
 
     public cat_jam_forge() {
         cat_jam.init();
+        initialiseModdedDiscs();
+    }
+
+    private void initialiseModdedDiscs() {
+
+        // Mod List
+
+        /* final boolean example_mod = ModList.get().isLoaded("example_mod"); */
+
+        // Modded Discs
+
+        /* if (example_mod) discManager.addDisc(new Disc("example_mod:example_disc", 0, 0)); */
+
     }
 
 }
