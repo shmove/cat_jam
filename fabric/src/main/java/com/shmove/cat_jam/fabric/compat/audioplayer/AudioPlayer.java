c248bb4927d3ff2b1abb2b701308517b58357384
shmove <leahlddl@gmail.com>
2023-06-28 22:09:48 +0100

    Fix null pointer exception (#6) (v1.1.2)

--- fabric/src/main/java/com/shmove/cat_jam/fabric/compat/audioplayer/AudioPlayer.java
+++ fabric/src/main/java/com/shmove/cat_jam/fabric/compat/audioplayer/AudioPlayer.java
@@ -1,41 +1,53 @@
 package com.shmove.cat_jam.fabric.compat.audioplayer;
 
 import com.shmove.cat_jam.cat_jam;
+import de.maxhenkel.voicechat.api.VoicechatPlugin;
+import de.maxhenkel.voicechat.api.events.EventRegistration;
+import de.maxhenkel.voicechat.api.events.OpenALSoundEvent;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.Vec3i;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class AudioPlayer {
+public class AudioPlayer implements VoicechatPlugin {
 
     public static final String DISC_SOUND_CATEGORY = "music_discs";
     public static final String CUSTOM_DISC_ID = "audioplayer:custom_disc";
     private static final int MAX_LIVELINESS = 5; // ticks to wait before considering a disc stopped
-    private static final HashMap<Vec3d, Integer> playbackLiveliness = new HashMap<>();
+    private static final HashMap<BlockPos, Integer> playbackLiveliness = new HashMap<>();
 
-    public static void pingPos(Vec3d pos) {
+    @Override
+    public String getPluginId() { return cat_jam.MOD_ID; }
+
+    @Override
+    public void registerEvents(EventRegistration registration) {
+        registration.registerEvent(OpenALSoundEvent.Pre.class, this::onSound);
+    }
+
+    private void onSound(OpenALSoundEvent.Pre sound) {
+        if (sound.getCategory() == null || sound.getPosition() == null) return;
+
+        if (sound.getCategory().equals(DISC_SOUND_CATEGORY))
+            pingPos(new BlockPos((int)Math.floor(sound.getPosition().getX()), (int)Math.floor(sound.getPosition().getY()), (int)Math.floor(sound.getPosition().getZ())));
+    }
+
+    public static void pingPos(BlockPos pos) {
         if (!playbackLiveliness.containsKey(pos))
-            cat_jam.jukeboxDiscUpdateEvent(MinecraftClient.getInstance().world, getBlockPos(pos), cat_jam.discManager.getDisc(CUSTOM_DISC_ID));
+            cat_jam.jukeboxDiscUpdateEvent(MinecraftClient.getInstance().world, pos, cat_jam.discManager.getDisc(CUSTOM_DISC_ID));
         playbackLiveliness.put(pos, MAX_LIVELINESS); // refresh liveliness
     }
 
     public static void tick(ClientWorld world) {
-        for (Vec3d pos : new ArrayList<>(playbackLiveliness.keySet())) {
+        for (BlockPos pos : new ArrayList<>(playbackLiveliness.keySet())) {
             if (playbackLiveliness.get(pos) > 0) {
                 playbackLiveliness.put(pos, playbackLiveliness.get(pos) - 1); // decrement liveliness
             } else {
                 playbackLiveliness.remove(pos); // remove if no longer playing
-                cat_jam.jukeboxDiscUpdateEvent(world, getBlockPos(pos), null);
+                cat_jam.jukeboxDiscUpdateEvent(world, pos, null);
             }
         }
-    }
-
-    private static BlockPos getBlockPos(Vec3d pos) {
-        return new BlockPos(new Vec3i((int)Math.floor(pos.x), (int)Math.floor(pos.y), (int)Math.floor(pos.z)));
     }
 
 }
